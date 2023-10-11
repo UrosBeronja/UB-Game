ef2eb52dd2dee7f90007ece8042765a9a3354ba1
Uros Beronja <uros.beronja2@gmail.com>
2023-10-12 01:37:00 +0200

    win/lose

--- UB Game/Main.cpp
+++ UB Game/Main.cpp
@@ -12,7 +12,6 @@ using namespace std;
 
 // Constants for the different moves
 
-
 const int ATTACK = 1;
 const int REVOLVER = 2;
 const int SHIELD = 3;
@@ -48,118 +47,125 @@ void handleMove(int move, Character& player, Character& enemy) {
     if (player.isAI == 1) {
         //AI generates its moves
         AIMove=enemy.do_ai_stuff();
-    }
-    if (move == 3) {
-        sh = 1;
-    }
+        if (move == 3) {
+            sh = 1;
+        }
+        //AI MOVES
 
+        if (AIMove == ATTACK) {
+            cout << "A.I. slices you with a sword." << endl;
+            enemy.hp -= 1 * (player.attackDmg * (enemy.shielded ? 0 : 1));
+            player.attackDmg = 1;
+            player.shielded = false;
+        }
+        else if (AIMove == FAN) {
+            if (player.shots < 1) {
+                cout << "A.I. doesnt have enough shots to Fan the Gun." << endl;
+                return;
+            }
+            cout << "A.I. Fans the Gun!" << endl;
+            while (player.shots != 0) {
+                enemy.hp -= 1.5 * (player.attackDmg * (sh ? 0 : 1));
+                player.shots -= 1;
+                cout << "A Shot hits you!" << endl;
+            }
+            player.attackDmg = 1;
+            player.shielded = false;
+        }
+        else if (AIMove == REVOLVER) {
+            if (player.shots < 1) {
+                cout << "A.I. doesn't have enough shots to use the Revolver." << endl;
+                return;
+            }
+            cout << "A.I. executes a devestating Shot!" << endl;
+            enemy.hp -= 1.5 * (player.attackDmg * (sh ? 0 : 1));
+            player.shots -= 1;
+            player.attackDmg = 1;
+            player.shielded = false;
+        }
 
-    if (AIMove == ATTACK) {
-        cout << "A.I. slices you with a sword." << endl;
-        player.hp -= 1 * (enemy.attackDmg * (sh ? 0 : 1));
-        enemy.attackDmg = 1;
-        enemy.shielded = false;
-    }
-    else if (AIMove == FAN) {
-        if (enemy.shots < 1) {
-            cout << "A.I. doesnt have enough shots to Fan the Gun." << endl;
-            return;
+        else if (AIMove == SHIELD) {
+            if (player.shieldUsed) {
+                cout << "You have already used your shield." << endl;
+                return;
+            }
+            cout << "You raised your shield." << endl;
+            player.shieldUsed = true;
+            player.shielded = true;
+            player.attackDmg = 2;
         }
-        cout << "A.I. Fans the Gun!" << endl;
-        while (enemy.shots != 0) {
-            player.hp -= 1.5 * (enemy.attackDmg * (sh ? 0 : 1));
-            enemy.shots -= 1;
-            cout << "A Shot hits you!" << endl;
+    }// Players Moves
+    else {
+        if (move == ATTACK) {
+            cout << "You sliced the A.I. with your sword." << endl;
+            enemy.hp -= 1 * (player.attackDmg * (enemy.shielded ? 0 : 1));
+            player.attackDmg = 1;
+            player.shielded = false;
         }
-        enemy.attackDmg = 1;
-        enemy.shielded = false;
-    }
-    else if (AIMove == REVOLVER) {
-        if (player.shots < 1) {
-            cout << "You don't have enough shots to use the Revolver." << endl;
-            return;
+        else if (move == FAN) {
+            if (player.shots < 1) {
+                cout << "You don't have enough shots to Fan the Gun." << endl;
+                return;
+            }
+            cout << "You Fan the Gun!" << endl;
+            while (enemy.shots != 0) {
+                player.hp -= 1.5 * (enemy.attackDmg * (sh ? 0 : 1));
+                enemy.shots -= 1;
+                cout << "A Shot hits the A.I.!" << endl;
+            }
+            enemy.attackDmg = 1;
+            enemy.shielded = false;
         }
-        cout << "You execute a devestating Shot!" << endl;
-        enemy.hp -= 1.5 * (player.attackDmg * (enemy.shielded ? 0 : 1));
-        player.shots -= 1;
-        player.attackDmg = 1;
-        player.shielded = false;
-    }
+        else if (move == REVOLVER) {
+            if (player.shots < 1) {
+                cout << "You don't have enough shots to use the Revolver." << endl;
+                return;
+            }
+            cout << "You execute a devestating Shot!" << endl;
+            enemy.hp -= 1.5 * (player.attackDmg * (enemy.shielded ? 0 : 1));
+            player.shots -= 1;
+            player.attackDmg = 1;
+            player.shielded = false;
+        }
 
-    else if (AIMove == SHIELD) {
-        if (player.shieldUsed) {
-            cout << "You have already used your shield." << endl;
-            return;
+        else if (move == SHIELD) {
+            if (player.shieldUsed) {
+                cout << "You have already used your shield." << endl;
+                return;
+            }
+            cout << "You raised your shield." << endl;
+            player.shieldUsed = true;
+            player.shielded = true;
+            player.attackDmg = 2;
         }
-        cout << "You raised your shield." << endl;
-        player.shieldUsed = true;
-        player.shielded = true;
-        player.attackDmg = 2;
-    }
-
-   
-    if (move == ATTACK) {
-        cout << "You sliced the enemy with your sword." << endl;
-        enemy.hp -= 1 * (player.attackDmg * (enemy.shielded ? 0 : 1));
-        player.attackDmg = 1;
-        player.shielded = false;
-    }
-    else if (move == FAN) {
-        if (player.shots < 15) {
-            cout << "You don't have enough shots to summon Ultima." << endl;
-            return;
-        }
-        cout << "You summoned Ultima!" << endl;
-        enemy.hp -= 10 * (player.attackDmg * (enemy.shielded ? 0 : 1));
-        player.shots -= 15;
-        player.attackDmg = 1;
-        player.shielded = false;
-    }
-    else if (move == REVOLVER) {
-        if (player.shots < 1) {
-            cout << "You don't have enough shots to use the Revolver." << endl;
-            return;
-        }
-        cout << "You execute a devestating Shot!" << endl;
-        enemy.hp -= 1.5 * (player.attackDmg * (enemy.shielded ? 0 : 1));
-        player.shots -= 1;
-        player.attackDmg = 1;
-        player.shielded = false;
-    }
-
-    else if (move == SHIELD) {
-        if (player.shieldUsed) {
-            cout << "You have already used your shield." << endl;
-            return;
-        }
-        cout << "You raised your shield." << endl;
-        player.shieldUsed = true;
-        player.shielded = true;
-        player.attackDmg = 2;
     }
 }
 
 void GameLoop(int move, Character player, Character enemy) {
+    bool exit;
     // Initialize characters, environment, and other game elements
 
     // Main game loop
     while (player.hp > 0 && enemy.hp > 0) {
         //processing the AI turn first, but giving it the players chosen move to apply the shied and dodge correctly
         handleMove(move, enemy, player);
-
         handleMove(move, player, enemy);
         
-        
-
-
-
-
+        if (enemy.hp < 0 && player.hp>0 ) {
+            exit = win(player, enemy);
+        } 
+        else if(player.hp < 0 && enemy.hp>0) {
+            exit = lose(player, enemy);
+        }
+        else if (player.hp == 0 && enemy.hp ==0) {
+            exit = lose(player, enemy);
+        }
     }
 
     // Display game outcome and handle game over logic
 }
 
-bool win(Character& player) {
+bool win(Character& player, Character& enemy) {
     string response;
     while (true) {
         cout << "You win. Would you like to play again? (yes/no)" << endl;
@@ -169,10 +175,16 @@ bool win(Character& player) {
         }
         if (response == "yes") {
             player.hp = 50;
+            enemy.hp = 50;
             player.shots = 6;
+            enemy.shots = 6;
             player.shieldUsed = false;
+            enemy.shieldUsed = false;
             player.shielded = false;
+            enemy.shielded = false;
             player.attackDmg = 1;
+            enemy.attackDmg = 1;
+
             return true;
         }
         else if (response == "no") {
@@ -184,7 +196,7 @@ bool win(Character& player) {
     }
 }
 
-bool lose(Character& player) {
+bool lose(Character& player, Character& enemy) {
     string response;
     while (true) {
         cout << "You lose. Would you like to play again? (yes/no)" << endl;
@@ -193,11 +205,16 @@ bool lose(Character& player) {
             c = tolower(c);
         }
         if (response == "yes") {
-            player.hp = 20;
+            player.hp = 50;
+            enemy.hp = 50;
             player.shots = 6;
+            enemy.shots = 6;
             player.shieldUsed = false;
+            enemy.shieldUsed = false;
             player.shielded = false;
+            enemy.shielded = false;
             player.attackDmg = 1;
+            enemy.attackDmg = 1;
             return true;
         }
         else if (response == "no") {
